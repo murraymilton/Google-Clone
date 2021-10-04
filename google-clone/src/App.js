f1d9b200fe5ec5035f3f57ee4b60b9abe965e91c
murraymilton <murray.l.milton@outlook.com>
2021-10-04 15:00:06 +0200

    added the initial homepage layout

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Google Clone</h1>
+      {/* Home()*/}
+      <Home />
+      {/* SearchPage(The results page)*/}
+
     </div>
   );
 }
---
+++ google-clone/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+           <h1>Homepage</h1>
+           <div className="home__header">
+                <div className="home__headerLeft">
+                    {/*About */}
+                    {/*Store */}
+
+                </div>
+                <div className="home__headerRight">
+                    {/*Gmail */}
+                    {/*Images */}
+                    {/*Icon */}
+                    {/*Avatar */}
+
+                </div>
+           </div>
+           <div className="home__body">
+
+           </div>
+        </div>
+    )
+}
+
+export default Home
