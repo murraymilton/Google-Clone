8bb9cc5770cc77c734a0fd5e4e1378fd473e6f11
murraymilton <murray.l.milton@outlook.com>
2021-10-04 15:46:57 +0200

    Added the main header to homepage

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import Home from './pages/Home';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Google Clone</h1>
-      {/* Home()*/}
-      <Home />
-      {/* SearchPage(The results page)*/}
-
+      <Router>
+        <Switch>
+          <Route exact path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- google-clone/src/pages/Home.jsx
+++ google-clone/src/pages/Home.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import AppIcon from "@material-ui/icons/Apps"
+import { Avatar } from '@material-ui/core'
 import './Home.css'
 function Home() {
     return (
         <div className="home">
-           <h1>Homepage</h1>
            <div className="home__header">
                 <div className="home__headerLeft">
-                    {/*About */}
-                    {/*Store */}
+                 <Link to="/about">About</Link>
+                 <Link to="/store">Store</Link>
 
                 </div>
                 <div className="home__headerRight">
+                 <Link to="/gmail">Gmail</Link>
+                 <Link to="/images">Images</Link>
+                 <AppIcon />
+                 <Avatar />
+                 {/* <Link to="/icon">About</Link>
+                 <Link to="/avatar">About</Link> */}
+                    
                     {/*Gmail */}
                     {/*Images */}
                     {/*Icon */}
