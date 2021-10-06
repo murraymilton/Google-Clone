888c91dacb448b4309ae3459d2571cafd4b12da2
murraymilton <murray.l.milton@outlook.com>
2021-10-06 19:39:36 +0200

    added usehistory for page re-direct to user search results page

--- google-clone/src/components/Search/Search.jsx
+++ google-clone/src/components/Search/Search.jsx
@@ -3,17 +3,20 @@ import './Search.css'
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import {Button} from "@material-ui/core";
-
+import { useHistory } from 'react-router-dom';
 function Search() {
     const [input, setInput] = useState("")
+    const history = useHistory();
     const search = e => {
         e.preventDefault();
         // Will prevent the page from reloading, will be the input value used to render user search results.
 
         //Added a console.log. To check the enter key response
-        console.log("The Google Search or Enter key was hit!!!! Show search page");
-        console.log(input)
-    }
+        console.log("The Google Search or Enter key was hit!!!! Show search page for >>>", input)
+
+        // Will re-direct user to the search page to view search results
+         history.push('/search')
+    };
     return (
         <form className="search">
             <div className="search__input">
